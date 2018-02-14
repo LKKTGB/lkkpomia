6a75389ab625f940b7dced11afb03e0b35ee4b56
badboy99tw <taiwan.badboy99@gmail.com>
2018-02-14 23:48:26 +0800

    Update Profile.avatar_url after login

--- website/settings/base.py
+++ website/settings/base.py
@@ -65,6 +65,18 @@ SOCIAL_AUTH_LOGIN_ERROR_URL = 'home'
 
 SOCIAL_AUTH_FACEBOOK_KEY = os.environ.get('SOCIAL_AUTH_FACEBOOK_KEY')
 SOCIAL_AUTH_FACEBOOK_SECRET = os.environ.get('SOCIAL_AUTH_FACEBOOK_SECRET')
+SOCIAL_AUTH_FACEBOOK_API_VERSION = '2.12'
+SOCIAL_AUTH_PIPELINE = (
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+    'website.pipeline.update_profile',
+)
 
 ROOT_URLCONF = 'website.urls'
 
