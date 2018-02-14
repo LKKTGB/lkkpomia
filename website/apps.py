b6a467cdd5f04692c6d2548d0fd35cc96d3e0f0e
badboy99tw <taiwan.badboy99@gmail.com>
2018-02-14 23:46:05 +0800

    Add model website.Profile

--- website/apps.py
+++ website/apps.py
@@ -5,3 +5,6 @@ from django.utils.translation import ugettext_lazy as _
 class WebsiteConfig(AppConfig):
     name = 'website'
     verbose_name = _('app_website')
+
+    def ready(self):
+        from website import signals
