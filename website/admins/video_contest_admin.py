5a3a2ec63c1f2571ec2eb0dae4140a776a7611a1
badboy99tw <taiwan.badboy99@gmail.com>
2018-02-16 03:15:46 +0800

    Add event models Event, Salon, VideoContest

---
+++ website/admins/video_contest_admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+
+class VideoContestAdmin(admin.ModelAdmin):
+
+    class Media:
+        js = [
+            'js/tinymce/tinymce.min.js',
+            'js/tinymce_settings.js',
+        ]
