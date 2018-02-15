645feef8143d9d5c91f37383136c71fadb4e81ca
badboy99tw <taiwan.badboy99@gmail.com>
2018-02-16 03:32:30 +0800

    Add model VideoContestGroup

--- website/admins/video_contest_admin.py
+++ website/admins/video_contest_admin.py
@@ -1,7 +1,20 @@
 from django.contrib import admin
 
+from website.models.video_contest_group import VideoContestGroup
+
+
+class VideoContestGroupInline(admin.StackedInline):
+    model = VideoContestGroup
+
+    extra = 0
+    classes = ('grp-collapse grp-open',)
+    inline_classes = ('grp-collapse grp-open',)
+
 
 class VideoContestAdmin(admin.ModelAdmin):
+    inlines = [
+        VideoContestGroupInline,
+    ]
 
     class Media:
         js = [
