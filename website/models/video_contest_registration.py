19601d2888b57408ef54035c9379af5af9303c30
badboy99tw <taiwan.badboy99@gmail.com>
2018-02-17 04:42:06 +0800

    Add and adjust fields of VideoContestRegistration * Add fields contestant_name, video_title, group * Change type of email to EmailField from CharField * Adjust verbose name of some fields * Add representation of model VideoContestGroup

--- website/models/video_contest_registration.py
+++ website/models/video_contest_registration.py
@@ -3,14 +3,19 @@ from django.utils.translation import ugettext_lazy as _
 from embed_video.fields import EmbedVideoField
 
 from website.models.registration import Registration
+from website.models.video_contest_group import VideoContestGroup
 
 
 class VideoContestRegistration(Registration):
     youtube_url = EmbedVideoField(_('video_contest_registration_youtube_url'))
     introduction = models.TextField(_('video_contest_registration_introduction'))
+    contestant_name = models.CharField(_('video_contest_registration_contestant_name'), max_length=100)
     phone_number = models.CharField(_('video_contest_registration_phone_number'), max_length=20)
     address = models.CharField(_('video_contest_registration_address'), max_length=100)
-    email = models.CharField(_('video_contest_registration_email'), max_length=100)
+    email = models.EmailField(_('video_contest_registration_email'))
+    video_title = models.CharField(_('video_contest_registration_video_title'), max_length=100)
+    group = models.ForeignKey(VideoContestGroup, on_delete=models.CASCADE,
+                              verbose_name=_('video_contest_registration_group'))
     questions = models.TextField(_('video_contest_registration_questions'), blank=True)
 
     qualified = models.BooleanField(_('video_contest_registration_qualified'), default=False)
