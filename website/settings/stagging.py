3ee60d760481cb6ab6996ba29c829e8a36d6aa24
badboy99tw <taiwan.badboy99@gmail.com>
2018-02-20 17:46:02 +0800

    Setup DB on Heroku

--- website/settings/stagging.py
+++ website/settings/stagging.py
@@ -1,6 +1,11 @@
+import dj_database_url
+
 from website.settings.base import *
 
 ALLOWED_HOSTS = ['*']
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (
