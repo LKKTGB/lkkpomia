b272784c76ea159f599d89cc2a9070c045f0a18c
badboy99tw <taiwan.badboy99@gmail.com>
2018-02-25 00:31:55 +0800

    fixup! Add proxy model UserProxy for custom text of field is_superuser

--- website/models/user_proxy.py
+++ website/models/user_proxy.py
@@ -9,4 +9,4 @@ class UserProxy(User):
 
 
 UserProxy._meta.get_field('is_superuser').verbose_name = _('user_proxy_is_superuser')
-UserProxy._meta.get_field('is_superuser').help_text = _('user_proxy_is_superuser_help_text'),
+UserProxy._meta.get_field('is_superuser').help_text = _('user_proxy_is_superuser_help_text')
