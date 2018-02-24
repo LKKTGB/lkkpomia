1d4e36f9118134c7290450bdfb1db9905446efda
badboy99tw <taiwan.badboy99@gmail.com>
2018-02-25 01:58:57 +0800

    Introduce WhiteNoise to support serving static files in production

--- website/settings/production.py
+++ website/settings/production.py
@@ -9,6 +9,12 @@ ALLOWED_HOSTS = [
     'lkk-website-production.herokuapp.com',
 ]
 
+# WhiteNoise
+MIDDLEWARE.extend([
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+])
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
