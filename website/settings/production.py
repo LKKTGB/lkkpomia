c280f3c79b29ca5fc7f51d6486146ce7e5b44a97
badboy99tw <taiwan.badboy99@gmail.com>
2018-02-25 01:46:28 +0800

    Add settings for production

--- website/settings/production.py
+++ website/settings/production.py
@@ -1,3 +1,18 @@
+import dj_database_url
+
 from website.settings.base import *
 
 DEBUG = False
+
+ALLOWED_HOSTS = [
+    'lkkpomia.tgb.org.tw',
+    'lkk-website-production.herokuapp.com',
+]
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
