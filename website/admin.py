f2ba4ee541317eefca4dbe6ac0aa604ce5a8c188
badboy99tw <taiwan.badboy99@gmail.com>
2018-05-13 20:31:47 +0800

    Refactor, import admin class from website.admins

--- website/admin.py
+++ website/admin.py
@@ -4,16 +4,7 @@ from social_django.models import Association, Nonce, UserSocialAuth
 from taggit.admin import Tag
 
 from website import models
-from website.admins.announcement_admin import AnnouncementAdmin
-from website.admins.event_admin import EventAdmin
-from website.admins.home_tab import HomeTabAdmin
-from website.admins.post_admin import PostAdmin
-from website.admins.privacy_policy_admin import PrivacyPolicyAdmin
-from website.admins.salon_admin import SalonAdmin
-from website.admins.salon_registration_admin import SalonRegistrationAdmin
-from website.admins.user_profile_admin import UserProfileAdmin
-from website.admins.video_contest_admin import VideoContestAdmin
-from website.admins.video_contest_registration_admin import VideoContestRegistrationAdmin
+from website import admins
 
 admin.site.unregister(Association)
 admin.site.unregister(Group)
@@ -21,13 +12,13 @@ admin.site.unregister(Nonce)
 admin.site.unregister(Tag)
 admin.site.unregister(User)
 admin.site.unregister(UserSocialAuth)
-# admin.site.register(models.Announcement, AnnouncementAdmin)
-# admin.site.register(models.Event, EventAdmin)
-admin.site.register(models.HomeTab, HomeTabAdmin)
-# admin.site.register(models.Post, PostAdmin)
-admin.site.register(models.PrivacyPolicy, PrivacyPolicyAdmin)
-admin.site.register(models.Salon, SalonAdmin)
-# admin.site.register(models.SalonRegistration, SalonRegistrationAdmin)
-admin.site.register(models.UserProxy, UserProfileAdmin)
-admin.site.register(models.VideoContest, VideoContestAdmin)
-admin.site.register(models.VideoContestRegistration, VideoContestRegistrationAdmin)
+# admin.site.register(models.Announcement, admins.AnnouncementAdmin)
+# admin.site.register(models.Event, admins.EventAdmin)
+admin.site.register(models.HomeTab, admins.HomeTabAdmin)
+# admin.site.register(models.Post, admins.PostAdmin)
+admin.site.register(models.PrivacyPolicy, admins.PrivacyPolicyAdmin)
+admin.site.register(models.Salon, admins.SalonAdmin)
+# admin.site.register(models.SalonRegistration, admins.SalonRegistrationAdmin)
+admin.site.register(models.UserProxy, admins.UserProfileAdmin)
+admin.site.register(models.VideoContest, admins.VideoContestAdmin)
+admin.site.register(models.VideoContestRegistration, admins.VideoContestRegistrationAdmin)
