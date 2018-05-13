88ba16da3ed0c851309d5214bf2706bec6d52322
badboy99tw <taiwan.badboy99@gmail.com>
2018-05-13 22:06:48 +0800

    Change parent class of view Post and Page

--- website/views/post.py
+++ website/views/post.py
@@ -1,17 +1,18 @@
 from django.conf import settings
 from django.urls import reverse
-from django.views.generic.detail import SingleObjectMixin
+from django.views.generic import DetailView
 
 from website.views.page import Page
 
 
-class Post(SingleObjectMixin, Page):
+class Post(Page, DetailView):
     '''
     View for general blog post, just text and images.
     '''
 
     def get_context_data(self, *args, **kwargs):
         context_data = super().get_context_data(*args, **kwargs)
+        context_data['login_modal'] = self.get_login_modal()
         context_data['meta_title'] = self.object.title,
         context_data['meta_tags'] = {
             'og:url': self.request.build_absolute_uri(reverse('post', args=(self.object.id,))),
