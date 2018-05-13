0aec91a995b87c1f3c6d113912f525a6daa2ad0c
badboy99tw <taiwan.badboy99@gmail.com>
2018-05-14 01:51:03 +0800

    Re-style posts page by CBV with Headline

--- website/views/__init__.py
+++ website/views/__init__.py
@@ -9,43 +9,6 @@ from website.views.salon import Salon
 from website.views.base import get_login_modal
 
 
-def get_header(request):
-    current_tab = request.GET.get('tab', None)
-
-    nav_items = []
-    nav_items.append({
-        'name': '全部活動',
-        'link': reverse('home'),
-        'current': current_tab is None
-    })
-    for tab in models.HomeTab.objects.order_by('order').all():
-        nav_items.append({
-            'name': tab.name,
-            'link': '%s?tab=%s' % (reverse('home'), tab.name),
-            'current': current_tab == tab.name
-        })
-    return {
-        'nav_items': nav_items,
-        'modal': get_login_modal(request)
-    }
-
-
-def home(request):
-    tab = request.GET.get('tab', None)
-    if tab:
-        posts = models.Post.objects.filter(tags__name__in=[tab])
-    else:
-        posts = models.Post.objects.all()
-
-    return render(request, 'posts.html', {
-        'meta_title': '李江却台語文教基金會',
-        'home': False,
-        'user': request.user,
-        'posts': posts,
-        'header': get_header(request)
-    })
-
-
 def post(request, post_id):
     try:
         post = models.Post.objects.get(id=post_id)
