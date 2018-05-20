d93f384bbfe6e2d8585a2393617059fb37f16bec
badboy99tw <taiwan.badboy99@gmail.com>
2018-05-20 12:16:22 +0800

    Add fields to SalonRegistration about contestant info

--- website/models/salon_registration.py
+++ website/models/salon_registration.py
@@ -1,9 +1,13 @@
+from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
 from website.models.registration import Registration
 
 
 class SalonRegistration(Registration):
+    contestant_name = models.CharField(_('salon_registration_contestant_name'), max_length=100)
+    phone_number = models.CharField(_('salon_registration_phone_number'), max_length=20, blank=True)
+    email = models.EmailField(_('salon_registration_email'), blank=True)
 
     class Meta:
         verbose_name = _('salon_registration')
