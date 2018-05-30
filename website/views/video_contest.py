63b44b605e744a909b8445b6671bb6b5ece3d4e8
badboy99tw <taiwan.badboy99@gmail.com>
2018-05-30 20:12:08 +0800

    Add search function to video page of VideoContest

--- website/views/video_contest.py
+++ website/views/video_contest.py
@@ -320,6 +320,10 @@ class Video(Page, DetailView):
             'title': self.video_contest.title,
             'url': reverse('post', kwargs={'post_id': self.video_contest.id})
         }
+        context_data['search'] = {
+            'target': reverse('gallery', kwargs={'post_id': self.video_contest.id}),
+            'placeholder': '搜尋影片'
+        }
         context_data['nav_items'] = get_nav_items(self.video_contest, self.request)
 
         videos = self.get_random_qualified_videos(max_count=10)
