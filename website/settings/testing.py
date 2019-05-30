0d56b889104df94edb37bea6ed045472d3457ed6
badboy99tw <taiwan.badboy99@gmail.com>
2019-05-30 17:38:28 +0800

    Apply whitenoise on testing to serve static files

--- website/settings/testing.py
+++ website/settings/testing.py
@@ -1,3 +1,6 @@
+"""Settings for testing environment deployed on Azure App Service
+"""
+
 import dj_database_url
 
 from website.settings.base import *
@@ -5,6 +8,10 @@ from website.settings.base import *
 ALLOWED_HOSTS = [
     'lkkpomia-testing.azurewebsites.net',
 ]
+
+# WhiteNoise
+MIDDLEWARE.extend(["whitenoise.middleware.WhiteNoiseMiddleware"])
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
